c8eb8cd60070a4490592e738890402d02cfc451b
abiola boluwatife <abiolaboluwatife@gmail.com>
2026-06-01 12:56:48 +0100

    chat app qualified

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
-export default {
+export default defineConfig({
   plugins: [
+    react(),
     VitePWA({
       registerType: "autoUpdate",
       manifest: {
@@ -17,22 +17,15 @@ export default {
           {
             src: "/icon-192.png",
             sizes: "192x192",
-            type: "image/png"
+            type: "image/png",
           },
           {
             src: "/icon-512.png",
             sizes: "512x512",
-            type: "image/png"
-          }
-        ]
-      }
-    })
-  ]
-};
-
-
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
